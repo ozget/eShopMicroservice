fce579b2692e7df47e937afa4d6d1b8dc4e80309
Ozge <Ozge@DESKTOP-ABOK54H>
2025-04-02 18:22:59 +0300

    Cqrs ,product handler,endpoint

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.Cqrs;
+using Catalog.Api.Models;
+using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name,List<string> Category,string Description, string ImageFile, decimal Price):ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    //CreateProductCommand, CreateProductResult tetiklenmesi icin  : ICommandHandler şeklinde yazdık
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+DependencyContextAssemblyCatalog dependencyContextAssemblyCatalog = new DependencyContextAssemblyCatalog([typeof(Program).Assembly]);
+
+builder.Services.AddCarter(dependencyContextAssemblyCatalog);
+
+
+builder.Services.AddMediatR(config =>
+{   //komut ve sorgularýn nerede olacagýný söyler
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+
+
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
 
 app.Run();
