54d4cae3c83b537d1aadcb8a8f51cb821329577c
Ozge <Ozge@DESKTOP-ABOK54H>
2025-05-27 13:38:02 +0300

    Grpc Discount Service

--- src/Services/Discount/DiscountGrpc/Program.cs
+++ src/Services/Discount/DiscountGrpc/Program.cs
@@ -1,23 +1,25 @@
+
+
+using DiscountGrpc.Data;
 using DiscountGrpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-builder.Services.AddGrpcReflection();
-
+builder.Services.AddGrpcReflection();//postman de service gözükmesi icin
+builder.Services.AddDbContext<DiscountContext>(opts =>
+        opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 
 
 var app = builder.Build();
 
-
-app.MapGrpcService<GreeterService>();
-
-if (app.Environment.IsDevelopment())
-
-    app.MapGrpcReflectionService();
-
-
+// Configure the HTTP request pipeline.
+app.UseMigration();
+app.MapGrpcService<DiscountService>();
+app.MapGrpcReflectionService();//postman de service gözükmesi icin
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
